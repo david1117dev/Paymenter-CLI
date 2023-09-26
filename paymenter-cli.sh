381fc80246bc30868fff1c39185351b25d8f1f1c
david1117dev <110995499+david1117dev@users.noreply.github.com>
2023-09-26 07:08:20 +0200

    Update paymenter-cli.sh

--- paymenter-cli.sh
+++ paymenter-cli.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Define text formatting escape codes (color placeholders)
 BLUE='\033[34m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-RESET='\e[0m' # Reset formatting
+NC='\033[0m'
+RESET='\e[0m'
 
-# Checkmark and cross symbols
 checkmark="${WHITE}[${GREEN}\xE2\x9C\x93${WHITE}] ${RESET}"
 cross="${WHITE}[${RED}\xE2\x9C\x97${WHITE}]${RESET}"
 ask="${WHITE}[${YELLOW}?${WHITE}]"
 
 
-# Function to display information
 main() {
   echo -e "${BLUE}   ____                                   __           "
   echo "  / __ \____ ___  ______ ___  ___  ____  / /____  _____"
@@ -33,33 +30,13 @@ main() {
   echo "  fix       - Fix common Paymenter issues"
 }
 
-# Function to install Paymenter
+
 install() {
   bash <(https://raw.githubusercontent.com/david1117dev/Paymenter-CLI/main/paymenter-install.sh)
-  echo "Installing Paymenter..."
-  # Add installation logic here
 }
 
-# Function to uninstall Paymenter
 uninstall() {
-  echo -e "${ask} Are you sure you want to uninstall Paymenter? (y/N): "
-  read -r confirm
-  if [[ "$confirm" =~ ^[Yy]$ ]]; then
-    echo "Uninstalling Paymenter..."
-    
-    # Uninstallation logic
-    mariadb --execute="DROP USER 'paymenter'; DROP DATABASE 'paymenter';"
-    rm -rf /var/www/paymenter/
-    rm -f /etc/nginx/sites-available/paymenter.conf
-    rm -f /etc/nginx/sites-enabled/paymenter.conf
-    (crontab -l | grep -v "* * * * * php /var/www/paymenter/artisan schedule:run >> /dev/null 2>&1") | crontab -
-    systemctl stop paymenter.service
-    rm -f /etc/systemd/system/paymenter.service
-    
-    echo -e "${checkmark} Paymenter has been successfully uninstalled."
-  else
-    echo "Uninstallation canceled."
-  fi
+  bash <(https://raw.githubusercontent.com/david1117dev/Paymenter-CLI/main/paymenter-uninstall.sh)
 }
 
 # Function to fix Paymenter issues
