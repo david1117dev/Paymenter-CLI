58bf09348e3768f5b24bde07ec7ce3565ad6f359
david1117dev <110995499+david1117dev@users.noreply.github.com>
2023-09-27 17:11:58 +0200

    Update paymenter-install.sh

--- paymenter-install.sh
+++ paymenter-install.sh
@@ -190,5 +190,5 @@ if [[ "$confirm" =~ ^[Yy]$ ]]; then
   setup_webserver
   setup_queue
 else
-  echo "${cross} Installation of paymenter canceled."
+  echo -e "${cross} Installation of paymenter canceled."
 fi
