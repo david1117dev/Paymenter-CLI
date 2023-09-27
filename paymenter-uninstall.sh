d8b7342676c9d453244600682bcc2229106ba39d
david1117dev <110995499+david1117dev@users.noreply.github.com>
2023-09-27 16:45:04 +0200

    Update paymenter-uninstall.sh

--- paymenter-uninstall.sh
+++ paymenter-uninstall.sh
@@ -8,15 +8,15 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 RESET='\e[0m'
 
-checkmark="${WHITE}[${GREEN}\xE2\x9C\x93${WHITE}] ${RESET}"
-cross="${WHITE}[${RED}\xE2\x9C\x97${WHITE}]${RESET}"
+checkmark="${WHITE}[${GREEN}\xE2\x9C\x93${WHITE}]"
+cross="${WHITE}[${RED}\xE2\x9C\x97${WHITE}]"
 ask="${WHITE}[${YELLOW}?${WHITE}]"
 uninstall(){
   echo -e "${ask} Are you sure you want to uninstall Paymenter? (y/N): "
   read -r confirm
   if [[ "$confirm" =~ ^[Yy]$ ]]; then
-    echo "Uninstalling Paymenter..."
-    mariadb --execute="DROP USER 'paymenter'; DROP DATABASE 'paymenter';"
+    echo "${checkmark} Uninstalling Paymenter..."
+    mariadb --execute="DROP USER IF EXISTS 'paymenter'@'127.0.0.1'; DROP DATABASE IF EXISTS 'paymenter';"
     rm -rf /var/www/paymenter/
     rm -f /etc/nginx/sites-available/paymenter.conf
     rm -f /etc/nginx/sites-enabled/paymenter.conf
@@ -26,7 +26,7 @@ uninstall(){
     
     echo -e "${checkmark} Paymenter has been successfully uninstalled."
   else
-    echo "Uninstallation canceled."
+    echo "${cross} Uninstallation canceled."
   fi
 }
 if [[ $EUID -ne 0 ]]; then
